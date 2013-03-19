24138f4ade07c62de9736d4f4df7f347cfa60e38
Michael Klishin <michael@defprotocol.org>
2013-03-20 03:53:21 +0400

    Depend on Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject zmaril/archimedes "0.2.0-SNAPSHOT"
-  :description "Clojure wrapper for blueprints"
-  :url "http://example.com/FIXME"
+  :description "Clojure wrapper for Tinkerpop Blueprints"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [potemkin "0.2.0"]                 
                  [zmaril/ogre "2.2.0.0"]
                  [com.tinkerpop.blueprints/blueprints-core "2.2.0"]])
