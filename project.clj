913c62013be7d8c299e54130189702de5f877e6f
Michael Klishin <michael@defprotocol.org>
2013-03-20 03:56:22 +0400

    Test against 1.4 and 1.6
    1.3 currently does not work and I think we should not worry
    about it too much

--- project.clj
+++ project.clj
@@ -5,4 +5,14 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [potemkin "0.2.0"]                 
                  [zmaril/ogre "2.2.0.0"]
-                 [com.tinkerpop.blueprints/blueprints-core "2.2.0"]])
+                 [com.tinkerpop.blueprints/blueprints-core "2.2.0"]]
+  :profiles {:1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6    {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
