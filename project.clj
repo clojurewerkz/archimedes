f0e4ff7bff1821e787443189bf057a10323a500b
Zack Maril <zack@zacharymaril.com>
2013-04-23 13:36:59 +0400

    Bump to alpha4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/archimedes "1.0.0-alpha3"
+(defproject clojurewerkz/archimedes "1.0.0-alpha4"
   :description "Clojure wrapper for Tinkerpop Blueprints"
   :url "https://github.com/clojurewerkz/archimedes"
   :license {:name "Eclipse Public License"
