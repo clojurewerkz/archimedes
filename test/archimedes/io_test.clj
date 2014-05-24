dd9b3796c59d44f902438f2b0a3f36fb528d31e3
Ray Miller <ray@1729.org.uk>
2014-05-24 15:40:37 +0100

    Passing I/O tests on humbled branch.

--- test/archimedes/io_test.clj
+++ test/archimedes/io_test.clj
@@ -4,117 +4,83 @@
             [archimedes.io :as io]
             [archimedes.vertex :as v]
             [archimedes.edge :as e]
-            [clojure.java.io :as clj-io]))
+            [clojure.java.io :as clj-io])
+  (:import [java.io File]))
 
-(defn has-n-vertices [n]
-  (is (= n (count (seq (.getVertices g/*graph*))))))
+(defn- has-n-vertices [graph n]
+  (is (= n (count (seq (.getVertices graph))))))
 
-(defn has-n-edges [n]
-  (is (= n (count (seq (.getEdges g/*graph*))))))
+(defn- has-n-edges [graph n]
+  (is (= n (count (seq (.getEdges graph))))))
+
+(defn- make-test-graph
+  []
+  (let [graph (g/clean-tinkergraph)
+        vertex-1 (v/create-with-id! graph 100)
+        vertex-2 (v/create-with-id! graph 101)
+        edge (e/connect-with-id! graph 102 vertex-1 :edge vertex-2)]
+    graph))
+
+(defn- make-test-graph-with-types
+  []
+  (let [graph (g/clean-tinkergraph)
+        vertex-1 (v/create-with-id! graph 100 {:my-int (int 1)
+                                               :my-long (long 2)
+                                               :my-float (float 3)
+                                               :my-double (double 4)
+                                               :my-boolean true})
+        vertex-2 (v/create-with-id! graph 101 {:my-int (int 10)
+                                               :my-long (long 20)
+                                               :my-float (float 30)
+                                               :my-double (double 40)
+                                               :my-boolean false})
+        edge (e/connect-with-id! graph 102 vertex-1 :edge vertex-2)]
+    graph))
 
 (deftest test-loading-and-saving-graphs-graphml
-  (g/use-clean-graph!)
-  (let [filename "my-test-graph.graphml"
-        file (clj-io/file filename)]
-    (letfn [(delete-graph-file [] (clj-io/delete-file file true))] ;; Delete file *silently* (no failure if it don't exist).
-      (delete-graph-file)
-      (let [vertex-1 (v/create-with-id! 100)
-            vertex-2 (v/create-with-id! 101)
-            edge (e/connect-with-id! 102 vertex-1 :edge vertex-2)]
-        (io/write-graph-graphml filename))
-
-      ;; Open new graph and read it
-      (g/use-clean-graph!)
-      (io/load-graph-graphml filename)
-
-      (has-n-vertices 2)
-      (has-n-edges 1)
-
-      (delete-graph-file))))
+  (let [graph (make-test-graph)
+        tmp   (File/createTempFile "my-test-graph" ".graphml")]
+    (io/write-graph-graphml graph tmp)
+    ;; Open new graph and read it
+    (let [graph2 (g/clean-tinkergraph)]
+      (io/load-graph-graphml graph2 tmp)
+      (has-n-vertices graph2 2)
+      (has-n-edges graph2 1))))
 
 (deftest test-loading-and-saving-graphs-gml
-  (g/use-clean-graph!)
-  (let [filename "my-test-graph.gml"
-        file (clj-io/file filename)]
-    (letfn [(delete-graph-file [] (clj-io/delete-file file true))] ;; Delete file *silently* (no failure if it don't exist).
-      (delete-graph-file)
-      (let [vertex-1 (v/create-with-id! 100)
-            vertex-2 (v/create-with-id! 101)
-            edge (e/connect-with-id! 102 vertex-1 :edge vertex-2)]
-        (io/write-graph-gml filename))
-
-      ;; Open new graph and read it
-      (g/use-clean-graph!)
-      (io/load-graph-gml filename)
-
-      (has-n-vertices 2)
-      (has-n-edges 1)
-
-      (delete-graph-file))))
+  (let [graph (make-test-graph)
+        tmp (File/createTempFile "my-test-graph" ".gml")]
+    (io/write-graph-gml graph tmp)
+    ;; Open new graph and read it
+    (let [graph2 (g/clean-tinkergraph)]
+      (io/load-graph-gml graph2 tmp)
+      (has-n-vertices graph2 2)
+      (has-n-edges graph2 1))))
 
 (deftest test-loading-and-saving-graphs-graphson
   (testing "Without type information"
-    (g/use-clean-graph!)
-    (let [filename "my-test-graph.graphson"
-          file (clj-io/file filename)]
-      (letfn [(delete-graph-file [] (clj-io/delete-file file true))] ;; Delete file *silently* (no failure if it don't exist).
-        (delete-graph-file)
-        (let [vertex-1 (v/create-with-id! 100)
-              vertex-2 (v/create-with-id! 101)
-              edge (e/connect-with-id! 102 vertex-1 :edge vertex-2)]
-          (io/write-graph-graphson filename))
-
-        ;; Open new graph and read it
-        (g/use-clean-graph!)
-        (io/load-graph-graphson filename)
-
-        (has-n-vertices 2)
-        (has-n-edges 1)
-
-        (delete-graph-file))))
+    (let [graph (make-test-graph)
+          tmp   (File/createTempFile "my-test-graph" ".graphson")]
+      (io/write-graph-graphson graph tmp)
+      ;; Open new graph and read it
+      (let [graph2 (g/clean-tinkergraph)]
+        (io/load-graph-graphson graph2 tmp)
+        (has-n-vertices graph2 2)
+        (has-n-edges graph2 1))))
 
   (testing "With a graph with type information"
-    (letfn [(init-graph-with-types []
-              (g/use-clean-graph!))]
-      (let [filename-typed "my-test-graph-typed.graphson"
-            filename-untyped "my-test-graph-untyped.graphson"
-            file-typed (clj-io/file filename-typed)
-            file-untyped (clj-io/file filename-untyped)]
-        (letfn [(delete-graph-files []
-                  (clj-io/delete-file file-typed true)
-                  (clj-io/delete-file file-untyped true))] ;; Delete files *silently* (no failure if it don't exist).
-
-          (delete-graph-files)
-          (init-graph-with-types)
-
-          (let [vertex-1 (v/create-with-id! 100 {:my-int (int 1)
-                                                 :my-long (long 2)
-                                                 :my-float (float 3)
-                                                 :my-double (double 4)
-                                                 :my-boolean true})
-                vertex-2 (v/create-with-id! 101 {:my-int (int 10)
-                                     :my-long (long 20)
-                                     :my-float (float 30)
-                                     :my-double (double 40)
-                                     :my-boolean false})
-                edge (e/connect-with-id! 102 vertex-1 :edge vertex-2)]
-
-                                        ; Write one with type info
-            (io/write-graph-graphson filename-typed true)
-
-                                        ; Write one without type info
-            (io/write-graph-graphson filename-untyped false))
-
-          ;; (testing "Loading a graphson without type infomation"
-          ;;   (init-graph-with-types)
-          ;;   (is (thrown? java.lang.IllegalArgumentException
-          ;;                (io/load-graph-graphson filename-untyped)) "Causes type errors to be thrown"))
-
-          (testing "Loading a graphson with type infomation"
-            (init-graph-with-types)
-            (io/load-graph-graphson filename-typed)
-
-            (has-n-vertices 2)
-            (has-n-edges 1))
-
-          (delete-graph-files))))))
+    (let [graph (make-test-graph-with-types)
+          tmp-typed (File/createTempFile "my-test-graph-typed" ".graphson")
+          tmp-untyped (File/createTempFile "my-test-graph-untyped" ".graphson")]
+      (io/write-graph-graphson graph tmp-typed true)
+      (io/write-graph-graphson graph tmp-untyped false)
+      (testing "Loading a graphson with type infomation"
+        (let [graph2 (g/clean-tinkergraph)]          
+          (io/load-graph-graphson graph2 tmp-typed)        
+          (has-n-vertices graph2 2)
+          (has-n-edges graph2 1)))
+      (testing "Loading a graphson without type infomation"
+        (let [graph2 (g/clean-tinkergraph)]          
+          (io/load-graph-graphson graph2 tmp-untyped)        
+          (has-n-vertices graph2 2)
+          (has-n-edges graph2 1))))))
