e735a6b559456fab60ae0a2b351578a0c529fcf2
Michael Klishin <michael@clojurewerkz.org>
2014-11-15 19:31:39 +0300

    Depend on Clojure 1.6

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojurewerkz/archimedes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [potemkin "0.2.0"]
                  [com.tinkerpop.blueprints/blueprints-core "2.5.0"]]
   :source-paths ["src/clojure"]
