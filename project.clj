bb870afb5b0e0c41c62128aea785da1f847e6c03
Michael Klishin <michael@clojurewerkz.org>
2014-11-15 19:32:21 +0300

    Drop Clojure 1.4

--- project.clj
+++ project.clj
@@ -13,10 +13,9 @@
                                      [clojurewerkz/support "1.0.0" :exclusions [com.google.guava/guava
                                                                                 org.clojure/clojure]]
                                      [commons-io/commons-io "2.4"]]}
-             :1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
